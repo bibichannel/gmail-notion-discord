6b412f227beb782412f6b375767e3898774bfe26
bibichannel <lytrung15901@gmail.com>
2023-06-12 08:56:34 +0700

    change sources

--- src/notion.py
+++ src/notion.py
@@ -15,10 +15,8 @@ class Notion:
         return response
 
     def post(self, num_pages=None):
-        """
-        If num_pages is None, get all pages, otherwise just the defined number.
-        """
-        url = f"https://api.notion.com/v1/databases/{self.database_id}/query"
+
+        url = f"{self.base_url}{self.database_id}/query"
 
         get_all = num_pages is None
         page_size = 100 if get_all else num_pages
@@ -31,58 +29,146 @@ class Notion:
         results = data["results"]
         while data["has_more"] and get_all:
             payload = {"page_size": page_size, "start_cursor": data["next_cursor"]}
-            url = f"https://api.notion.com/v1/databases/{self.database_id}/query"
             response = requests.post(url, json=payload, headers=self.header)
             data = response.json()
             results.extend(data["results"])
-
         return results
-   
+      
     def dictionary_decoder(self, response):
         return [i for i in response]
 
-
 # ------------------------------------------------------------------------
 
-class PropertiesNotion(Notion):
-    # Properly of database Notion
-
+class TaskPageProperties():
     def __init__(self):
-        self.id = random.randint(5000, 5500)
-        self.assign = []
-        self.ticket = ""
-        self.note = ""
-        self.link = ""
-        self.owner = ""
-        self.status = ""
+        '''
+        ['URL', 'Report', 'Owner', 'Last edited by', 'Summary', 
+        'JIRA Status', 'Last edited time', 'Evidence', 'Task name', 
+        'Tags', 'Assignee', 'Status', 'Due', 'Priority', 'Sub-tasks', 
+        'Estimates', 'Parent-task', 'Feature']
+        '''
+        self.id = random.randint(2000, 5000)
+        self.page_id = None
+        self.page_url = None
+        self.jira_status= None
+        self.status = None
+        self.task_name = None
+        self.url_jira = None
+        self.owner = None
+        self.report = None
+        self.summary = None
+        self.estimates = None
+        self.evidence = None
+        self.priority = None
+        self.tags = None
+        self.feature = None
+        self.parent_task = None
+        self.sub_tasks = None
+        self.assignee = None
 
-    def check_ticket_assgin(self, response, ticket):
+    def get_data(self, pages):
 
-        for pages in response:
-            if pages["properties"]["Ticket"]["title"] != []:
-                self.ticket = pages["properties"]["Ticket"]["title"][0]["plain_text"]
+        self.page_id = pages['id']
 
-            if ticket in self.ticket:
-                if pages["properties"]["Note"]["rich_text"] != []:
-                    self.note = pages["properties"]["Note"]["rich_text"][0]["plain_text"]
+        self.page_url = pages['url']
 
-                if pages["properties"]["Link"]["formula"] != []:
-                    self.link = pages["properties"]["Link"]["formula"]["string"]
+        self.jira_status = pages['properties']['JIRA Status']['status']['name']
 
-                if pages["properties"]["Owner"]["select"] is not None:
-                    self.owner = pages["properties"]["Owner"]["select"]["name"]
+        self.status =  pages['properties']['Status']['status']['name']
 
-                if pages["properties"]["Status"]["status"] is not None:
-                    self.status = pages["properties"]["Status"]["status"]["name"]
+        if pages["properties"]["Task name"]["title"]:
+            self.task_name = pages['properties']["Task name"]["title"][0]["plain_text"]
+        
+        if pages['properties']['URL']['url']:
+            self.url_jira = pages['properties']['URL']['url']
 
-                if pages["properties"]['Assignee']['multi_select'] != []:
-                    for j in range(len(pages["properties"]["Assignee"]["multi_select"])):
-                        assign_user = pages["properties"]["Assignee"]["multi_select"][j]["name"]
-                        self.assign.append(assign_user)
+        if pages['properties']['Owner']['select']:
+            self.owner = pages['properties']['Owner']['select']['name']
+        
+        if pages['properties']['Report']['select']:
+            self.report = pages['properties']['Report']['select']['name']
 
-                return True
+        if pages['properties']['Summary']['rich_text']:
+            self.summary = pages['properties']['Summary']['rich_text'][0]['plain_text']
+
+        if pages['properties']['Estimates']['select']:
+            self.estimates = pages['properties']['Estimates']['select']['name']
+
+        if pages['properties']['Evidence']['files']:
+            self.evidence = pages['properties']['Evidence']['files']
+
+        if pages['properties']['Priority']['select']:
+            self.priority = pages['properties']['Priority']['select']['name']
+
+        if pages['properties']['Tags']['multi_select']:
+            self.tags = [i['name'] for i in pages['properties']['Tags']['multi_select']]
+
+        if pages['properties']['Feature']['relation']:
+            self.feature = [i['id'] for i in pages['properties']['Feature']['relation']]
+        
+        if pages['properties']['Parent-task']['relation']:
+            self.parent_task = pages['properties']['Parent-task']['relation'][0]['id']
+        
+        if pages['properties']['Sub-tasks']['relation']:
+            self.sub_tasks = [i['id'] for i in pages['properties']['Sub-tasks']['relation']]
+        
+        if pages['properties']['Assignee']['people']:
+            self.assignee = [i['person']['email'] for i in pages['properties']['Assignee']['people']]
+        
+        
+    def check_ticket_assgin(self, ticket):
+
+        if ticket in self.task_name:
+            return True
         return False
     
+
+class FeaturePageProperties():
+
+    def __init__(self):
+
+        '''['Project name', 'Owner', 'Status', 'Completion', 
+        'Priority', 'Dates', 'Tasks', 'Is Blocking', 'Blocked By']'''
+
+        self.id = random.randint(6000, 9000)
+        self.page_url = None
+        self.page_id = None
+        self.project_name = None
+        self.owner = None
+        self.status = None
+        self.completion = None
+        self.priority = None
+        self.is_blocking = None
+        self.block_by = None
+
+    def get_data(self, pages):
+
+        self.page_id = pages['id']
+
+        self.page_url = pages['url']
+
+        self.status = pages['properties']['Status']['status']['name']
+
+        if pages['properties']['Project name']['title']:
+            self.project_name = pages['properties']['Project name']['title'][0]['plain_text']
+
+        if pages['properties']['Owner']['people']:
+            self.owner = [i['person']['email'] for i in pages['properties']['Owner']['people']]
+
+        percent = pages['properties']['Completion']['rollup']['number'] if pages['properties']['Completion']['rollup']['number'] else 0
+        #self.completion = f"{round(percent*100, 2)}%"
+        self.completion = "{:.0f}%".format(round(percent*100))
+
+        if pages['properties']['Priority']['select']:
+            self.priority = pages['properties']['Priority']['select']['name']
+
+        if pages['properties']['Is Blocking']['relation']:
+            self.is_blocking = [i['id'] for i in pages['properties']['Is Blocking']['relation']]
+
+        if pages['properties']['Blocked By']['relation']:
+            self.block_by = [i['id'] for i in pages['properties']['Blocked By']['relation']]
+
+
 # --------------------------------------------------------------
 # data noramilize to dict: {<ticket>:<status>}
 def data_normalize_by_status(response, status_name):
